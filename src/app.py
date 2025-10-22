3724c6e57f4de6f34d8229ab385e651c0602cfd6
Itsmeashok <athapach@emeal.nttdata.com>
2025-10-22 23:52:50 +0200

    -

--- src/app.py
+++ src/app.py
@@ -16,7 +16,10 @@ def details():
 @app.route('/api/v1/healthz')
 
 def health():
-    return jsonify({ 'status':'up' }), 200
+    return jsonify({ 
+        'status':'up' ,
+        'message': 'Hello, World!'
+        }), 200
 
 if __name__ == '__main__':
 
